c8da2094febebc425f9c6859035bd7d39518a56a
Zahragoli <95707310+Zahragoli1998@users.noreply.github.com>
2021-12-12 18:42:31 +0330

    flower

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, {Component} from "react"
+import './style.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    constructor(){
+        super()
+        this.state={
+            inputsearch:'',
+            flowers:[
+                {id:1,
+                name:'Aconite',
+                image:'flower/1.jpg'},
+                {id:2,
+                name:'African Daisy',
+                image:'flower/2.jpg'},
+                {id:3,
+                name:'Bee Balm',
+                image:'flower/3.jpg'},
+                {id:4,
+                name:'Coreopsis',
+                image:'flower/4.jpg'},
+                {id:5,
+                name:'Crown Imperial',
+                image:'flower/5.jpg'},
+                {id:6,
+                name:'Daffodil',
+                image:'flower/6.jpg'},
+                {id:7,
+                name:'Poppy',
+                image:'flower/7.jpg'},
+                {id:8,
+                name:'Sage',
+                image:'flower/8.jpg'},
+                {id:9,
+                name:'Scilla',
+                image:'flower/9.jpg'},
+                {id:10,
+                name:'Sunflower',
+                image:'flower/10.jpg'}
+                
+           ]
+        }
+    }
+    render(){
+        const filter =this.state.flowers.filter((text)=>{
+            return(
+               text.name.toLowerCase().includes(this.state.inputsearch.toLowerCase()))
+            })
+        return(  
+          <div className="boxes">
+                <input placeholder='Search here.....' className="input" value={this.state.inputsearch} onChange={(e) =>{
+                 this.setState({inputsearch:e.target.value})
+                 }}/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+              <div className="box">
+                     {
+                      filter.map((item) => {
+                      return(
+                      <div className={item.id} id="card">
+                      <div><img className="image" src ={item.image}/></div>
+                      <div><h2>{item.name}</h2></div>
+                      </div> )}
+                       )
+                     }
+             </div> 
+         </div>   
+        
+        )
+    }
 }
-
 export default App;
